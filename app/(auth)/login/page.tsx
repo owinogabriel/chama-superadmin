3d9f05d44862d231b5b81b2c36e0a145e9faffa4
owinogabriel <g73346920@gmail.com>
2026-02-20 08:54:27 +0300

    feat: initial project setup with auth foundation

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/component/auth/Login";
+import { Card, CardHeader, CardTitle } from "@/component/ui/Card";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+ <div className="">
+    <LoginForm />
+    </div>
+  );
+}
